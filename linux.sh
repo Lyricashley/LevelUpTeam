b7f08ede13b26bb31e7984ee0ad496efe14a7f8a
Lyricashley <lyric.thomp@gmail.com>
2023-05-04 02:40:53 +0000

    Git er Done

--- linux.sh
+++ linux.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-echo "Please enter your name:"
-read name
+echo " Please enter your name:"
+read Lyric T 
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:"Lyricashley" 
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:"May 3, 2023"
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:"
-echo "-----------------------------"
+echo "--------------------44.197.182.194---------"
 sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
+
